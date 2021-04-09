d17dab45c2b37cd83e8d370734e4f01aa20f6f9e
gurudasKarale <67753744+GurudasKarale@users.noreply.github.com>
2021-04-09 16:31:54 +0530

    Update settings.py

--- tododrf/tododrf/settings.py
+++ tododrf/tododrf/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'rest_database',
         'USER':'postgres',
-        'PASSWORD':'digital8956',
+        'PASSWORD':'',
         'HOST':'localhost',
     }
 }
